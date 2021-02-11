1d528fd7d3b710328766f6a9ee0d014f2a58e5fb
Richelle Soares <richelle.soares@gmail.com>
2021-02-11 16:42:13 -0300

    Treino em .NET

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace dotnet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+         int numeroDeVezes = 5;
+         for (int i = 0; i <= numeroDeVezes; i++)
+            {
+                Console.WriteLine($"Bem vindo ao Curso de .NET {i}");
+            }
         }
     }
 }
